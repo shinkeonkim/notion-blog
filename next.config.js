79b4a35dfa9eba342c4ef1a884af8186e93307b8
shinkeonkim <singun11@kookmin.ac.kr>
2022-06-01 11:21:47 +0900

    chore: gh-pages를 통한 배포

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: (process.env.NODE_ENV === 'production' ? 'http://www.singun11.wtf/notion-blog' : '')
 }
 
 module.exports = nextConfig
